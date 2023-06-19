1d509b62a2996401b108dbf07902a8b0430b5814
pichai.panklom <pichai.panklom@gmail.com>
2023-06-19 22:33:52 +0700

    Initial Commit

---
+++ Percentage Calculator/Percentage_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Percentage_CalculatorApp.swift
+//  Percentage Calculator
+//
+//  Created by Pichai Panklom on 6/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Percentage_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
